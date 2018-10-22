9a3381d3d1347856446f91c63f8a5456b9a27676
Rocco Panacci <rocco@clausehound.com>
2018-10-22 17:33:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dice Game
+
+Complete the steps in the HTML and JS files to develop the first part of a dice game interface.
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Step 2: Randomize a dice roll (make sure it's an integer number)
+
+
+// Step 3: Update the user interface (document) to show the diceface (svg image)
+
+
+// Step 4: Convert the roll number (for example: 3) to a word ("three")
