c7bb9415c089b8dcefc15645733b3d55866efef0
Rocco <rp@Roccos-iMac.local>
2020-11-24 11:09:42 -0500

    start of conditions

--- js/dice.js
+++ js/dice.js
@@ -1,4 +1,4 @@
 // This function returns a random Number between/including 1 and "sides" (defaults to 6)
-function getRandomDiceRoll(sides=6) {
+let getRandomDiceRoll = function(sides = 6) {
   return Math.floor( Math.random() * sides ) + 1
 }
--- js/script.js
+++ js/script.js
@@ -1,67 +1,21 @@
-// Search for the .msg
-let theMsg = document.querySelector('.msg')
-// Search for the .face <img>
-let theFace = document.querySelector('.face')
-// Search for the .dice <div>
-// let theDice = document.querySelector('.dice')
+let theMsg = document.querySelector('.msg') // .msg <div>
+let theFace = document.querySelector('.face') // .face <img>
+
 
 
 let rollTheDice = function() {
-
-  // Roll a dice... we got 5
+  // Roll a dice (1-6)
   let diceRoll = getRandomDiceRoll()
 
-  // Replace the textContent with "You rolled: 4"
+  // Update the message
   theMsg.textContent = `You rolled: ${diceRoll}`
 
-
-  // OPTION 1:
-  // Replace the src attribute with "img/dice4.svg"
+  // Update the diceface image
   theFace.setAttribute(`src`, `img/dice${diceRoll}.svg`)
-
-  // Replace the alt attribute with "Rolled 4"
   theFace.setAttribute(`alt`, `Rolled ${diceRoll}`)
-
-  /*
-  // OPTION 2:
- 
-  // Replace the innerHTML <img> with a new <img>
-  theDice.innerHTML = `<img src="img/dice${diceRoll}.svg" alt="Rolled ${diceRoll}" class="face">`
-  */
 }
 
 
-let theBtn = document.querySelector(`.roll`)
+let theBtn = document.querySelector(`.roll`)  // .roll <button>
+theBtn.addEventListener(`click`, rollTheDice)  // Add an event listener
 
-// EVENT: Something that happens (for example: click)
-// EVENT LISTENER: When an event occurs, here's what we need to do
-
-// Where will this event occur?      theBtn (.roll)
-// What type of event will it be?    `click`
-// What do we do, when it occurs?    rollTheDice
-
-theBtn.addEventListener(`click`, rollTheDice)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
-
-// 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
-
-// 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
-
-// 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
