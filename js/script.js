57abc9c482eb860ae498b63ad89f9bbc7410dc83
Rocco <rp@Roccos-iMac.local>
2020-11-24 10:56:41 -0500

    first version of the solutution

--- js/script.js
+++ js/script.js
@@ -1,5 +1,62 @@
-// Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
+// Search for the .msg
+let theMsg = document.querySelector('.msg')
+// Search for the .face <img>
+let theFace = document.querySelector('.face')
+// Search for the .dice <div>
+// let theDice = document.querySelector('.dice')
+
+
+let rollTheDice = function() {
+
+  // Roll a dice... we got 5
+  let diceRoll = getRandomDiceRoll()
+
+  // Replace the textContent with "You rolled: 4"
+  theMsg.textContent = `You rolled: ${diceRoll}`
+
+
+  // OPTION 1:
+  // Replace the src attribute with "img/dice4.svg"
+  theFace.setAttribute(`src`, `img/dice${diceRoll}.svg`)
+
+  // Replace the alt attribute with "Rolled 4"
+  theFace.setAttribute(`alt`, `Rolled ${diceRoll}`)
+
+  /*
+  // OPTION 2:
+ 
+  // Replace the innerHTML <img> with a new <img>
+  theDice.innerHTML = `<img src="img/dice${diceRoll}.svg" alt="Rolled ${diceRoll}" class="face">`
+  */
+}
+
+
+let theBtn = document.querySelector(`.roll`)
+
+// EVENT: Something that happens (for example: click)
+// EVENT LISTENER: When an event occurs, here's what we need to do
+
+// Where will this event occur?      theBtn (.roll)
+// What type of event will it be?    `click`
+// What do we do, when it occurs?    rollTheDice
+
+theBtn.addEventListener(`click`, rollTheDice)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
 
