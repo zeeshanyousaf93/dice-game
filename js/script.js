ccef6a46ba916f2e732e0dc7a72348ba35024d09
zeeshanyousaf93 <zeeshanyousaf93@gmail.com>
2021-04-12 20:07:27 -0400

    Dice function added

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 // If no argument is passed, 6 is set, ie, returns a number between and including 1 to 6
 // 		For example: 
 //    getRandomDiceRoll() will return either 1, 2, 3, 4, 5, 6
-const getRandomDiceRoll = function(sides=6) {
-  return Math.floor( Math.random() * sides ) + 1
+const getRandomDiceRoll = function (sides = 6) {
+  return Math.floor(Math.random() * sides) + 1
 }
 
 // Here's a test roll, check the console!
@@ -16,3 +16,15 @@ console.log(getRandomDiceRoll())
 // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
 
 // 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
+
+
+const rollTheDice = function () {
+  var diceRoll = getRandomDiceRoll();
+
+  document.getElementsByTagName('body')[0].innerHTML = `
+<p>You rolled: ${diceRoll}</p>
+<img src='./img/dice${diceRoll}.svg' height='200' width='200'/>
+`;
+}
+
+rollTheDice();
